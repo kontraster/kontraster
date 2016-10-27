ad768e297486faf2f074cc853a550e7a494dcd6f
Tim Severien <timseverien@gmail.com>
2016-10-27 19:11:26 +0200

    Make gulp watch reload CSS instead of refreshing the page

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,6 +4,17 @@ const browserSync = require('browser-sync').create();
 const config = require('./config');
 const defaultPort = require('../package').config.port;
 
+/**
+ * Updates browser assets
+ */
+function update() {
+  return gulp.src(`${config.paths.styles.destination}/**/*.css`)
+      .pipe(browserSync.stream());
+}
+
+/**
+ * Reloads the browser page
+ */
 function reload(done) {
   browserSync.reload();
   done();
@@ -16,5 +27,5 @@ module.exports = () => {
 
   gulp.watch(`${config.paths.scripts.source}/**/*.js`, gulp.series('scripts', reload));
   gulp.watch(`${config.paths.shaders.source}/**/*.glsl`, gulp.series('shaders', reload));
-  gulp.watch(`${config.paths.styles.source}/**/*.scss`, gulp.series('styles', reload));
+  gulp.watch(`${config.paths.styles.source}/**/*.scss`, gulp.series('styles', update));
 };
