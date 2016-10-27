6c636aae87f6935874ecda6bec60db76b9d16103
Tim Severien <timseverien@gmail.com>
2016-10-27 17:52:57 +0200

    Add basic style

--- gulp/styles.js
+++ gulp/styles.js
@@ -7,8 +7,12 @@ const sourcemaps = require('gulp-sourcemaps');
 const config = require('./config');
 
 module.exports = () => gulp.src(`${config.paths.styles.source}/**/*.scss`)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(config.environment.production ? minifyCss() : util.noop())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(config.paths.styles.destination));
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    includePaths: [
+      'node_modules/normalize.scss',
+    ],
+  }))
+  .pipe(config.environment.production ? minifyCss() : util.noop())
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest(config.paths.styles.destination));
