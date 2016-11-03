90f0f633d686412a1f0e5754905fd7364bdc6824
Tim Severien <timseverien@gmail.com>
2016-11-03 22:07:01 +0100

    Only compress images when production flag is enabled

--- gulp/images.js
+++ gulp/images.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp');
+const util = require('gulp-util');
 const minifyImage = require('gulp-imagemin');
 const config = require('./config');
 
 module.exports = () => gulp.src(`${config.paths.images.source}/**/*.{gif,jpg,png,svg}`)
-  .pipe(minifyImage())
+  .pipe(config.environment.production ? minifyImage() : util.noop())
   .pipe(gulp.dest(config.paths.images.destination));
